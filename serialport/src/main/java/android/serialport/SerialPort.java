be8a99f435e7c14652abbf4dc406e15ceba54728
licheedev <licheedev@foxmail.com>
2017-09-18 20:10:55 +0800

    修复bug，添加可以修改su路径

--- serialport/src/main/java/android/serialport/SerialPort.java
+++ serialport/src/main/java/android/serialport/SerialPort.java
@@ -16,6 +16,7 @@
 
 package android.serialport;
 
+import android.util.Log;
 import java.io.File;
 import java.io.FileDescriptor;
 import java.io.FileInputStream;
@@ -24,62 +25,89 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-import android.util.Log;
-
 public class SerialPort {
 
-	private static final String TAG = "SerialPort";
+    private static final String TAG = "SerialPort";
 
-	/*
-	 * Do not remove or rename the field mFd: it is used by native method close();
-	 */
-	private FileDescriptor mFd;
-	private FileInputStream mFileInputStream;
-	private FileOutputStream mFileOutputStream;
+    private static final String DEFAULT_SU_PATH = "/system/bin/su";
 
-	public SerialPort(File device, int baudrate, int flags) throws SecurityException, IOException {
+    private static String sSuPath = DEFAULT_SU_PATH;
+
+    /**
+     * Set the su binary path, the default su binary path is {@link #DEFAULT_SU_PATH}
+     *
+     * @param suPath su binary path
+     */
+    public static void setSuPath(String suPath) {
+        if (suPath == null) {
+            return;
+        }
+        sSuPath = suPath;
+    }
+
+    /*
+         * Do not remove or rename the field mFd: it is used by native method close();
+         */
+    private FileDescriptor mFd;
+    private FileInputStream mFileInputStream;
+    private FileOutputStream mFileOutputStream;
+
+    public SerialPort(File device, int baudrate, int flags) throws SecurityException, IOException {
 
 		/* Check access permission */
-		if (!device.canRead() || !device.canWrite()) {
-			try {
-				/* Missing read/write permission, trying to chmod the file */
-				Process su;
-				su = Runtime.getRuntime().exec("/system/bin/su");
-				String cmd = "chmod 666 " + device.getAbsolutePath() + "\n"
-						+ "exit\n";
-				su.getOutputStream().write(cmd.getBytes());
-				if ((su.waitFor() != 0) || !device.canRead()
-						|| !device.canWrite()) {
-					throw new SecurityException();
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-				throw new SecurityException();
-			}
-		}
+        if (!device.canRead() || !device.canWrite()) {
+            try {
+                /* Missing read/write permission, trying to chmod the file */
+                Process su;
+                su = Runtime.getRuntime().exec(sSuPath);
+                String cmd = "chmod 666 " + device.getAbsolutePath() + "\n" + "exit\n";
+                su.getOutputStream().write(cmd.getBytes());
+                if ((su.waitFor() != 0) || !device.canRead() || !device.canWrite()) {
+                    throw new SecurityException();
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                throw new SecurityException();
+            }
+        }
 
-		mFd = open(device.getAbsolutePath(), baudrate, flags);
-		if (mFd == null) {
-			Log.e(TAG, "native open returns null");
-			throw new IOException();
-		}
-		mFileInputStream = new FileInputStream(mFd);
-		mFileOutputStream = new FileOutputStream(mFd);
-	}
+        mFd = open(device.getAbsolutePath(), baudrate, flags);
+        if (mFd == null) {
+            Log.e(TAG, "native open returns null");
+            throw new IOException();
+        }
+        mFileInputStream = new FileInputStream(mFd);
+        mFileOutputStream = new FileOutputStream(mFd);
+    }
 
-	// Getters and setters
-	public InputStream getInputStream() {
-		return mFileInputStream;
-	}
+    public SerialPort(String devicePath, int baudrate, int flags)
+        throws SecurityException, IOException {
+        this(new File(devicePath), baudrate, flags);
+    }
 
-	public OutputStream getOutputStream() {
-		return mFileOutputStream;
-	}
+    public SerialPort(File device, int baudrate) throws SecurityException, IOException {
+        this(device, baudrate, 0);
+    }
 
-	// JNI
-	private native static FileDescriptor open(String path, int baudrate, int flags);
-	public native void close();
-	static {
-		System.loadLibrary("serial_port");
-	}
+    public SerialPort(String devicePath, int baudrate) throws SecurityException, IOException {
+        this(new File(devicePath), baudrate, 0);
+    }
+
+    // Getters and setters
+    public InputStream getInputStream() {
+        return mFileInputStream;
+    }
+
+    public OutputStream getOutputStream() {
+        return mFileOutputStream;
+    }
+
+    // JNI
+    private native static FileDescriptor open(String path, int baudrate, int flags);
+
+    public native void close();
+
+    static {
+        System.loadLibrary("serial_port");
+    }
 }
--- serialport/src/main/java/android/serialport/SerialPortFinder.java
+++ serialport/src/main/java/android/serialport/SerialPortFinder.java
@@ -16,6 +16,7 @@
 
 package android.serialport;
 
+import android.util.Log;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
@@ -23,100 +24,105 @@ import java.io.LineNumberReader;
 import java.util.Iterator;
 import java.util.Vector;
 
-import android.util.Log;
-
 public class SerialPortFinder {
 
-	public class Driver {
-		public Driver(String name, String root) {
-			mDriverName = name;
-			mDeviceRoot = root;
-		}
-		private String mDriverName;
-		private String mDeviceRoot;
-		Vector<File> mDevices = null;
-		public Vector<File> getDevices() {
-			if (mDevices == null) {
-				mDevices = new Vector<File>();
-				File dev = new File("/dev");
-				File[] files = dev.listFiles();
-				int i;
-				for (i=0; i<files.length; i++) {
-					if (files[i].getAbsolutePath().startsWith(mDeviceRoot)) {
-						Log.d(TAG, "Found new device: " + files[i]);
-						mDevices.add(files[i]);
-					}
-				}
-			}
-			return mDevices;
-		}
-		public String getName() {
-			return mDriverName;
-		}
-	}
+    public class Driver {
+        public Driver(String name, String root) {
+            mDriverName = name;
+            mDeviceRoot = root;
+        }
 
-	private static final String TAG = "SerialPort";
+        private String mDriverName;
+        private String mDeviceRoot;
+        Vector<File> mDevices = null;
 
-	private Vector<Driver> mDrivers = null;
+        public Vector<File> getDevices() {
+            if (mDevices == null) {
+                mDevices = new Vector<File>();
+                File dev = new File("/dev");
+                
+                File[] files = dev.listFiles();
 
-	Vector<Driver> getDrivers() throws IOException {
-		if (mDrivers == null) {
-			mDrivers = new Vector<Driver>();
-			LineNumberReader r = new LineNumberReader(new FileReader("/proc/tty/drivers"));
-			String l;
-			while((l = r.readLine()) != null) {
-				// Issue 3:
-				// Since driver name may contain spaces, we do not extract driver name with split()
-				String drivername = l.substring(0, 0x15).trim();
-				String[] w = l.split(" +");
-				if ((w.length >= 5) && (w[w.length-1].equals("serial"))) {
-					Log.d(TAG, "Found new driver " + drivername + " on " + w[w.length-4]);
-					mDrivers.add(new Driver(drivername, w[w.length-4]));
-				}
-			}
-			r.close();
-		}
-		return mDrivers;
-	}
+                if (files != null) {
+                    int i;
+                    for (i = 0; i < files.length; i++) {
+                        if (files[i].getAbsolutePath().startsWith(mDeviceRoot)) {
+                            Log.d(TAG, "Found new device: " + files[i]);
+                            mDevices.add(files[i]);
+                        }
+                    }
+                }
+            }
+            return mDevices;
+        }
 
-	public String[] getAllDevices() {
-		Vector<String> devices = new Vector<String>();
-		// Parse each driver
-		Iterator<Driver> itdriv;
-		try {
-			itdriv = getDrivers().iterator();
-			while(itdriv.hasNext()) {
-				Driver driver = itdriv.next();
-				Iterator<File> itdev = driver.getDevices().iterator();
-				while(itdev.hasNext()) {
-					String device = itdev.next().getName();
-					String value = String.format("%s (%s)", device, driver.getName());
-					devices.add(value);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return devices.toArray(new String[devices.size()]);
-	}
+        public String getName() {
+            return mDriverName;
+        }
+    }
 
-	public String[] getAllDevicesPath() {
-		Vector<String> devices = new Vector<String>();
-		// Parse each driver
-		Iterator<Driver> itdriv;
-		try {
-			itdriv = getDrivers().iterator();
-			while(itdriv.hasNext()) {
-				Driver driver = itdriv.next();
-				Iterator<File> itdev = driver.getDevices().iterator();
-				while(itdev.hasNext()) {
-					String device = itdev.next().getAbsolutePath();
-					devices.add(device);
-				}
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return devices.toArray(new String[devices.size()]);
-	}
+    private static final String TAG = "SerialPort";
+
+    private Vector<Driver> mDrivers = null;
+
+    Vector<Driver> getDrivers() throws IOException {
+        if (mDrivers == null) {
+            mDrivers = new Vector<Driver>();
+            LineNumberReader r = new LineNumberReader(new FileReader("/proc/tty/drivers"));
+            String l;
+            while ((l = r.readLine()) != null) {
+                // Issue 3:
+                // Since driver name may contain spaces, we do not extract driver name with split()
+                String drivername = l.substring(0, 0x15).trim();
+                String[] w = l.split(" +");
+                if ((w.length >= 5) && (w[w.length - 1].equals("serial"))) {
+                    Log.d(TAG, "Found new driver " + drivername + " on " + w[w.length - 4]);
+                    mDrivers.add(new Driver(drivername, w[w.length - 4]));
+                }
+            }
+            r.close();
+        }
+        return mDrivers;
+    }
+
+    public String[] getAllDevices() {
+        Vector<String> devices = new Vector<String>();
+        // Parse each driver
+        Iterator<Driver> itdriv;
+        try {
+            itdriv = getDrivers().iterator();
+            while (itdriv.hasNext()) {
+                Driver driver = itdriv.next();
+                Iterator<File> itdev = driver.getDevices().iterator();
+                while (itdev.hasNext()) {
+                    String device = itdev.next().getName();
+                    String value = String.format("%s (%s)", device, driver.getName());
+                    devices.add(value);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return devices.toArray(new String[devices.size()]);
+    }
+
+    public String[] getAllDevicesPath() {
+        Vector<String> devices = new Vector<String>();
+        // Parse each driver
+        Iterator<Driver> itdriv;
+        try {
+            itdriv = getDrivers().iterator();
+            while (itdriv.hasNext()) {
+                Driver driver = itdriv.next();
+                Iterator<File> itdev = driver.getDevices().iterator();
+                while (itdev.hasNext()) {
+                    String device = itdev.next().getAbsolutePath();
+                    devices.add(device);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return devices.toArray(new String[devices.size()]);
+    }
 }
